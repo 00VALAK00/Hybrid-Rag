76d7c04dc503e18e8c7150243ff9a49ee4eff4dd
00VALAK <iheb.abdsslem@eniso.u-sousse.tn>
2025-10-08 17:25:20 +0100

    Changed model

--- src/utils/chat_model.py
+++ src/utils/chat_model.py
@@ -1,7 +1,16 @@
 from langchain.chat_models import init_chat_model
+import os 
+from dotenv import load_dotenv
+from pathlib import Path
+root_dir = Path(__file__).resolve().parent.parent.parent
+load_dotenv(str(root_dir / ".env") )
 
 
-llm = init_chat_model(model_provider = "ollama",
-                    model= "gemma2:9b",
-                    temperature=0.7, 
-                    max_retries=3)
+    
+API_KEY = os.getenv("GEMINI_API","")
+llm = init_chat_model(model_provider="google_genai",
+                        model= "gemini-2.0-flash",
+                        temperature=0.7, 
+                        max_retries=3,
+                        api_key=API_KEY
+                        )
